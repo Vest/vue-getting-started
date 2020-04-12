f1b45570e7e66ebb3fbfefe7f0be319968be3ff9
Vest <Vest@users.noreply.github.com>
2020-04-12 12:06:10 +0200

    Chapter 04 Selecting and Highlighting the Item p.s. the selectedHero model is undefined (thus broken)
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- chapter0402/src/components/heroes.vue
+++ chapter0402/src/components/heroes.vue
@@ -10,7 +10,11 @@
         </header>
         <ul class="list is-hoverable">
           <li v-for="hero in heroes" :key="hero.id">
-            <a class="list-item">
+            <a
+              class="list-item"
+              @click="selectedHero = hero"
+              :class="{ 'is-active': selectedHero == hero }"
+            >
               <span>{{ hero.firstName }}</span>
             </a>
           </li>
@@ -66,12 +70,7 @@ export default {
   name: 'Heroes',
   data() {
     return {
-      selectedHero: {
-        id: 111,
-        firstName: '...',
-        lastName: '...',
-        description: '...',
-      },
+      selectedHero: undefined,
       heroes: [
         {
           id: 10,
