f285ed5c56f8a1120432c4f7ff3a8b45d70398f4
Vest <Vest@users.noreply.github.com>
2020-04-13 11:18:34 +0200

    Chapter 06 Parsing responses and error handling
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- chapter0702/src/shared/data.js
+++ chapter0702/src/shared/data.js
@@ -5,13 +5,32 @@ import * as axios from 'axios';
 import { API } from './config';
 
 const getHeroes = async function() {
-  const response = await axios.get(`${API}/heroes.json`);
-  const heroes = response.data.map(h => {
-    h.originDate = format(h.originDate, inputDateFormat);
-    return h;
-  });
+  try {
+    const response = await axios.get(`${API}/heroes.json`);
+    let data = parseList(response);
+    const heroes = data.map(h => {
+      h.originDate = format(h.originDate, inputDateFormat);
+      return h;
+    });
 
-  return heroes;
+    return heroes;
+  } catch (error) {
+    console.error(`Cannot get the response from ${API}/heroes.json: ${error}`);
+    return [];
+  }
+};
+
+const parseList = response => {
+  if (response.status !== 200) {
+    throw new Error(response.message);
+  }
+  if (!response.data) return [];
+  let list = response.data;
+  if (typeof list !== 'object') {
+    list = [];
+  }
+
+  return list;
 };
 
 export const data = {
