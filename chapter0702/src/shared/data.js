912717e279e450b622d7b80bcfefac8ffba476a2
Vest <Vest@users.noreply.github.com>
2020-04-13 11:03:25 +0200

    Chapter 06 Getting data for different environments
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- chapter0702/src/shared/data.js
+++ chapter0702/src/shared/data.js
@@ -2,8 +2,10 @@ import { format } from 'date-fns';
 import { inputDateFormat } from './constants';
 import * as axios from 'axios';
 
+import { API } from './config';
+
 const getHeroes = async function() {
-  const response = await axios.get('api/heroes.json');
+  const response = await axios.get(`${API}/heroes.json`);
   const heroes = response.data.map(h => {
     h.originDate = format(h.originDate, inputDateFormat);
     return h;
