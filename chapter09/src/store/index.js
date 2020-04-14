8a74249e63b151fb8d52b85df2ca6a5491d5ebc8
Vest <Vest@users.noreply.github.com>
2020-04-14 09:57:08 +0200

    Chapter 09 Mutating State;
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- chapter09/src/store/index.js
+++ chapter09/src/store/index.js
@@ -1,24 +1,42 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import { dataService } from '@/shared';
-import { GET_HEROES } from '@/store/mutation-types';
+import {
+  ADD_HERO,
+  DELETE_HERO,
+  GET_HEROES,
+  UPDATE_HERO,
+} from '@/store/mutation-types';
 
 Vue.use(Vuex);
 
 const state = {
   heroes: [],
 };
+
 const mutations = {
   [GET_HEROES](state, heroes) {
     state.heroes = heroes;
   },
+  [ADD_HERO](state, hero) {
+    state.heroes.push(hero);
+  },
+  [DELETE_HERO](state, hero) {
+    state.heroes = [...state.heroes.filter(h => h.id !== hero.id)];
+  },
+  [UPDATE_HERO](state, hero) {
+    const index = state.heroes.findIndex(h => h.id === hero.id);
+    state.heroes.splice(index, 1, hero);
+  },
 };
+
 const actions = {
   async getHeroesAction({ commit }) {
     const heroes = await dataService.getHeroes();
     commit(GET_HEROES, heroes);
   },
 };
+
 const getters = {
   getHeroById: state => id => state.heroes.find(h => h.id === id),
 };
